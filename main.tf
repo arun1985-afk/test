50699b38998253e9c3a03bffe3455a741dee6885
arun1985-afk <59602569+arun1985-afk@users.noreply.github.com>
2020-01-23 13:24:41 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-04590e7389a6e577c"
   instance_type = "t2.micro"
-  subnet        = "subnet-00158c49bb7aa8673"
-
+ 
   tags = {
     Name = "tfinstance"
   }
