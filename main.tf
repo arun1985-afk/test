be0e3d9d5c774abd71adf34b475f0dc2d80a1ac2
arun1985-afk <59602569+arun1985-afk@users.noreply.github.com>
2020-01-23 20:07:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-04590e7389a6e577c"
   instance_type = "t2.medium"
+  subnet_id     = "subnet-01c6a1c7298d181ed"
  
   tags = {
     Name = "tfinstance"
