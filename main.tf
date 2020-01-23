19bfeb276d19500117d40c22b7e4b5c3aa4ea12a
arun1985-afk <59602569+arun1985-afk@users.noreply.github.com>
2020-01-23 13:22:10 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,12 +4,11 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-04590e7389a6e577c"
   instance_type = "t2.micro"
+  subnet        = "subnet-00158c49bb7aa8673"
 
   tags = {
     Name = "tfinstance"
   }
 }
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.1.0/24"
-}
+
 
