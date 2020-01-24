f48e27c81a2709593bc12f8b836ec93dff3ea32c
arun1985-afk <59602569+arun1985-afk@users.noreply.github.com>
2020-01-24 12:21:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,8 +6,6 @@ resource "aws_instance" "web" {
   ami           = "ami-03d87256af0650d55"
   instance_type = "t2.micro"
   subnet_id     = "subnet-00158c49bb7aa8673"
-  vpc_security_group_ids = ["sg-0838b201c63605c88"]
-  associate_public_ip_address = true
  
   tags = {
     Name = "tfinstance"
