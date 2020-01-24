fc6fc4416d87da18face7c9776815c7e8aac85e2
arun1985-afk <59602569+arun1985-afk@users.noreply.github.com>
2020-01-24 12:10:47 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,20 @@
 provider "aws" {
   region = "us-west-2"
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "all" {
+  vpc_id = "vpc-0f889ec8acff2d9d0"
+}
 resource "aws_instance" "web" {
-  ami           = "ami-04590e7389a6e577c"
-  instance_type = "t2.micro"
-  subnet_id     = "subnet-01c6a1c7298d181ed"
+  ami           = "ami-03d87256af0650d55"
+  instance_type = "t2.small"
+  subnet_id     = "subnet-00158c49bb7aa8673"
+  vpc_security_group_ids = ["sg-0838b201c63605c88"]
+  associate_public_ip_address = true
  
   tags = {
     Name = "tfinstance"
